6751ee36a3708bfadbf05badfbfd6d4555137330
Pablo_HD <phdamini@hotmail.com>
2021-06-19 22:25:57 -0300

    Add Todo

--- Teste_SQL/school.sql
+++ Teste_SQL/school.sql
@@ -1,25 +1,25 @@
 --Buscar os dias da semana que tenham aulas da disciplina 'MATEMATICA'.
-SELECT 
+SELECT
 	t.dia_da_semana
-FROM 
-	turma t 
-JOIN 
-	disciplina d 
+FROM
+	turma t
+JOIN
+	disciplina d
 	ON d.nome = 'MATEMATICA'
 
 --Buscar todos os alunos que frequentem aulas de 'MATEMATICA' e também 'FISICA'.
 
-SELECT 
+SELECT
 	ALUNO.Nome
-FROM 
-	(SELECT * FROM  ALUNO_TURMA) 
-JOIN 
+FROM
+	(SELECT * FROM  ALUNO_TURMA)
+JOIN
 	ALUNO
 	ON ALUNO.id = ALUNO_TURMA.aluno_id
-JOIN 
+JOIN
 	TURMA
 	ON TURMA.id = ALUNO_TURMA.turma_id
-JOIN 
+JOIN
 	DISCIPLINA
 	ON TURMA.disciplina_id = DISCIPLINA.id
 	WHERE DISCIPLINA.nome = 'MATEMATICA' AND WHERE DISCIPLINA.nome = 'FISICA' 
